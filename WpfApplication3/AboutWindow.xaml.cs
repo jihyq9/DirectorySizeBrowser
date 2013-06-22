1e6bcab57ee4c86786e9f0d44728d04195a50bf8
JIH <anonymouse.237@gmail.com>
2013-06-22 18:41:20 -0400

    cleanup

---
+++ WpfApplication3/AboutWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace DirectorySizeBrowser
+{
+    /// <summary>
+    /// Interaction logic for AboutWindow.xaml
+    /// </summary>
+    public partial class AboutWindow : Window
+    {
+        public AboutWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OkayButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void GitLink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+        }
+    }
+}
